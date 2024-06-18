17dc1d8775f4071fec2c4c4c4fb950ef098892e4
Saneel Prabhu <hello@saneelprabhu.com>
2024-06-18 20:54:10 +0000

    fix int transactions summary  join logic

--- models/intermediate/int_transactions_summarized.sql
+++ models/intermediate/int_transactions_summarized.sql
@@ -10,22 +10,34 @@ select *
 from {{ ref('stg_transaction_line') }}
 ), 
 
-transactions_summarized as (
+transactions_grouped as (
 SELECT 
     t.location_id,
     t.bin_id,
     t.inventory_status_id,
     t.item_id,
     t.transaction_date,
-    sum(t.quantity) as quantity,
-    sum(c.cost) as cost 
+    sum(t.quantity) as ending_quantity
 from transaction_line_items as t 
-left join temp_costs as c 
+group by 1,2,3,4,5
+),
+
+transactions_summarized as (
+select 
+    t.location_id,
+    t.bin_id,
+    t.inventory_status_id,
+    t.item_id,
+    t.transaction_date,
+    t.ending_quantity,
+    (t.ending_quantity * c.cost) as ending_value 
+from transactions_grouped as t 
+inner join temp_costs as c 
     ON t.item_id = c.item_id 
     AND t.location_id = c.location_id 
     AND t.transaction_date between c.effective_start_date and c.effective_end_date 
-group by 1,2,3,4,5
-)
+) 
 
 select * 
 from transactions_summarized
+
